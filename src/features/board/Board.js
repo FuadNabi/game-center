6602c76474fbf61e8d522f0ddd2dc175f496c7f7
FuadNabi <fuad.nebiyev95@mail.ru>
2023-08-07 13:57:32 +0400

    Add function for generate board

--- src/features/board/Board.js
+++ src/features/board/Board.js
@@ -1,6 +1,21 @@
+import React from 'react';
+
 function Board() {
+  const rows = [];
+
+  for (let i = 0; i < 8; i += 1) {
+    const cells = [];
+    for (let j = 0; j < 8; j += 1) {
+      cells.push(<div key={`${i}-${j}`}>[]</div>);
+    }
+    rows.push(<div key={i}>{cells}</div>);
+  }
+
   return (
-    <h1>checkers</h1>
+    <main>
+      <h1>Checkers</h1>
+      <div className="board-container">{rows}</div>
+    </main>
   );
 }
 
